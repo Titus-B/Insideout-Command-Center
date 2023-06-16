af4d8d667f1e513d59947e2c64ad5be055582422
Titus-B <58118976+Titus-B@users.noreply.github.com>
2023-06-15 21:07:57 -0400

    Add files via upload

---
+++ Wav_File.ino
@@ -0,0 +1,37 @@
+#include "SD.h"
+#define SD_ChipSelectPin 4
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+void setup(){
+tmrpcm.speakerPin = 9;
+Serial.begin(9600);
+if (!SD.begin(SD_ChipSelectPin)) {
+Serial.println("SD fail");
+return;
+}
+
+char fname = "ANGER_1.wav";
+Serial.println(fname);
+
+// Check to see if the file exists:
+
+  if (SD.exists(fname)) {
+
+    Serial.println(" exists.");
+
+  } else {
+
+    Serial.println(" doesn't exist.");
+
+  }
+
+tmrpcm.setVolume(20);
+tmrpcm.play(fname);
+Serial.println("finish audio setup");
+
+}
+
+void loop(){  }
