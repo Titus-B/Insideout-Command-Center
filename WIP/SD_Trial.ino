30097872bb84159ca8217f999639485723e1a136
Titus-B <58118976+Titus-B@users.noreply.github.com>
2023-06-18 20:28:20 -0400

    Update SD_Trial.ino

--- WIP/SD_Trial.ino
+++ WIP/SD_Trial.ino
@@ -1,121 +1,117 @@
-/// File:       SD_Trial
-/// Version:    Draft Version 0.1
-/// Resourses:  https://docs.arduino.cc/learn/programming/sd-guide#file-management (Public Domain)
+/*
+  SD card test
 
-// Include Files
-#include <SPI.h>        //Serial Periperal Interface
-#include <SD.h>         //SD Library
-#include <TMRpcm.h>     //include speaker control library
+///Can Read files in the root
+  This example shows how use the utility libraries on which the'
+  SD library is based in order to get info about your SD card.
+  Very useful for testing a card when you're not sure whether its working or not.
+
+  The circuit:
+    SD card attached to SPI bus as follows:
+ ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
+ ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
+ ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
+ ** CS - depends on your SD card shield or module.
+ 		Pin 4 used here for consistency with other Arduino examples
+
+
+  created  28 Mar 2011
+  by Limor Fried
+  modified 9 Apr 2012
+  by Tom Igoe
+*/
+// include the SD library:
+#include <SPI.h>
+#include <SD.h>
 
 // set up variables using the SD utility library functions:
 Sd2Card card;
 SdVolume volume;
 SdFile root;
-const int chipSelect = 10;  //define CS pin
 
-//const char *wav_files[10] = { "1.wav", "2.wav", "3.wav", "4.wav", "5.wav", "6.wav", "7.wav", "8.wav", "9.wav", "10.wav" };
-
-TMRpcm sound;                            //crete an object for speaker library
-
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// MKRZero SD: SDCARD_SS_PIN
+const int chipSelect = 4;
 
 void setup() {
-
- // Open serial communications and wait for port to open:
-    Serial.begin(9600);
-    // wait for Serial Monitor to connect. Needed for native USB port boards only:
-    while (!Serial);
-    Serial.print("1 of 4 Initializing SD card via Begin...");
-    if (!SD.begin(chipSelect)) {
-        Serial.println("initialization failed. Things to check:");
-        Serial.println("1. is a card inserted?");
-        Serial.println("2. is your wiring correct?");
-        Serial.println("3. did you change the chipSelect pin to match your shield or module?");
-        Serial.println("Note: press reset or reopen this serial monitor after fixing your issue!");
-    }
-    Serial.println("initialization done.");
-
-    Serial.print("2 of 4 Initializing SD card...");
-    if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-        Serial.println("initialization failed. Things to check:");
-        Serial.println("* is a card inserted?");
-        Serial.println("* is your wiring correct?");
-        Serial.println("* did you change the chipSelect pin to match your shield or module?");
-        while (1);
-    } else {
-        Serial.print("Wiring is correct and a card is present...");
-    }
-    // print the type of card
-        Serial.print("Card type: ");
-        Serial.print(card.type());
-        Serial.print("... OR ");
-        switch (card.type()) {
-            case SD_CARD_TYPE_SD1:
-            Serial.print("SD1");
-            break;
-            case SD_CARD_TYPE_SD2:
-            Serial.print("SD2");
-            break;
-            case SD_CARD_TYPE_SDHC:
-            Serial.print("SDHC");
-            break;
-            default:
-            Serial.print("Unknown");
-        }
-
-    Serial.println("...initialization done.");
-
-    // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-    Serial.print("3 of 5 Explore Volume Formatting...");
-    if (!volume.init(card)) {
-        Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-        while (1);
-    }
-    uint32_t volumesize;
-    Serial.print("...Volume type is:    FAT");
-    Serial.print(volume.fatType(), DEC);
-    volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-    volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-    volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-    Serial.print("... Volume size (Kb):  ");
-    Serial.print(volumesize);
-
-    Serial.print("4 of 5 Select root directory...");
-    //root = SD.open("/");
-    root.openRoot(volume);
-    Serial.println("done!");
-    
-    Serial.print("5 of 5 Print Root Directory...");
-    //printDirectory(root, 0);
-    // list all files in the card with date and size
-    root.ls(LS_R | LS_DATE | LS_SIZE);
-    root.close();
-    Serial.println("done!");
-}
-
-void loop() {
-  // nothing happens after setup finishes.
-}
-
-void printDirectory(File dir, int numTabs) {
-
-  while (true) {
-    File entry =  dir.openNextFile();
-    if (! entry) {
-      // no more files
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++) {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    } else {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
   }
+
+
+  Serial.print("\nInitializing SD card...");
+
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.println();
+  Serial.print("Card type:         ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    while (1);
+  }
+
+  Serial.print("Clusters:          ");
+  Serial.println(volume.clusterCount());
+  Serial.print("Blocks x Cluster:  ");
+  Serial.println(volume.blocksPerCluster());
+
+  Serial.print("Total Blocks:      ");
+  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  Serial.println();
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("Volume type is:    FAT");
+  Serial.println(volume.fatType(), DEC);
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
+  Serial.print("Volume size (Kb):  ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mb):  ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Gb):  ");
+  Serial.println((float)volumesize / 1024.0);
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+}
+
+void loop(void) {
 }
